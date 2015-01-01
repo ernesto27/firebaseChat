291def26d091830685d0c0f699dc8ebaace9470a
Ernesto <ernestoponce27@gmail.com>
2015-01-01 13:58:22 -0300

    Add basic setup

---
+++ src/js/main.js
@@ -0,0 +1,152 @@
+  // 
+// Here is how to define your module 
+// has dependent on mobile-angular-ui
+// 
+var app = angular.module('MobileAngularUiExamples', [
+  'ngRoute',
+  'mobile-angular-ui',
+  
+  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+  // it is at a very beginning stage, so please be careful if you like to use
+  // in production. This is intended to provide a flexible, integrated and and 
+  // easy to use alternative to other 3rd party libs like hammer.js, with the
+  // final pourpose to integrate gestures into default ui interactions like 
+  // opening sidebars, turning switches on/off ..
+  'mobile-angular-ui.gestures',
+
+  // If you're migrating from Mobile Angular UI 1.1 and you are using 
+  // 'mobile-angular-ui.migrate.js' you have to require it too
+  'mobile-angular-ui.migrate'
+]);
+
+app.constant("PATHTEMPLATES", "src/js/partials/");
+
+// 
+// You can configure ngRoute as always, but to take advantage of SharedState location
+// feature (i.e. close sidebar on backbutton) you should setup 'reloadOnSearch: false' 
+// in order to avoid unwanted routing.
+// 
+app.config(function($routeProvider, PATHTEMPLATES) {
+  $routeProvider.when('/', {templateUrl: 'home.html', reloadOnSearch: false});
+  $routeProvider.when('/login', {
+    controller: "LoginCtrl",
+    templateUrl: PATHTEMPLATES + 'login.html', 
+    reloadOnSearch: false
+  });
+
+  $routeProvider.when('/scroll',        {templateUrl: 'scroll.html', reloadOnSearch: false}); 
+  $routeProvider.when('/toggle',        {templateUrl: 'toggle.html', reloadOnSearch: false}); 
+  $routeProvider.when('/tabs',          {templateUrl: 'tabs.html', reloadOnSearch: false}); 
+  $routeProvider.when('/accordion',     {templateUrl: 'accordion.html', reloadOnSearch: false}); 
+  $routeProvider.when('/overlay',       {templateUrl: 'overlay.html', reloadOnSearch: false}); 
+  $routeProvider.when('/forms',         {templateUrl: 'forms.html', reloadOnSearch: false});
+  $routeProvider.when('/dropdown',      {templateUrl: 'dropdown.html', reloadOnSearch: false});
+  $routeProvider.when('/drag',          {templateUrl: 'drag.html', reloadOnSearch: false});
+  $routeProvider.when('/carousel',      {templateUrl: 'carousel.html', reloadOnSearch: false});
+  $routeProvider.when('/toggleLegacy',  {templateUrl: 'toggleLegacy.html', reloadOnSearch: false});
+});
+
+
+
+app.controller("LoginCtrl", function($scope){
+  $scope.doLogin = function(){
+    console.log("Go to login")
+  }
+});
+
+
+
+//
+// For this trivial demo we have just a unique MainController 
+// for everything
+//
+app.controller('MainController', function($rootScope, $scope){
+
+  // User agent displayed in home page
+  $scope.userAgent = navigator.userAgent;
+  
+  // Needed for the loading screen
+  $rootScope.$on('$routeChangeStart', function(){
+    $rootScope.loading = true;
+  });
+
+  $rootScope.$on('$routeChangeSuccess', function(){
+    $rootScope.loading = false;
+  });
+
+  // Fake text i used here and there.
+  $scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo, aliquid eaque soluta nihil eligendi adipisci error, illum corrupti nam fuga omnis quod quaerat mollitia expedita impedit dolores ipsam. Obcaecati.';
+
+  // 
+  // 'Scroll' screen
+  // 
+  var scrollItems = [];
+
+  for (var i=1; i<=100; i++) {
+    scrollItems.push('Item ' + i);
+  }
+
+  $scope.scrollItems = scrollItems;
+
+  $scope.bottomReached = function() {
+    alert('Congrats you scrolled to the end of the list!');
+  }
+
+  // 
+  // Right Sidebar
+  // 
+  $scope.chatUsers = [
+    { name: 'Carlos  Flowers', online: true },
+    { name: 'Byron Taylor', online: true },
+    { name: 'Jana  Terry', online: true },
+    { name: 'Darryl  Stone', online: true },
+    { name: 'Fannie  Carlson', online: true },
+    { name: 'Holly Nguyen', online: true },
+    { name: 'Bill  Chavez', online: true },
+    { name: 'Veronica  Maxwell', online: true },
+    { name: 'Jessica Webster', online: true },
+    { name: 'Jackie  Barton', online: true },
+    { name: 'Crystal Drake', online: false },
+    { name: 'Milton  Dean', online: false },
+    { name: 'Joann Johnston', online: false },
+    { name: 'Cora  Vaughn', online: false },
+    { name: 'Nina  Briggs', online: false },
+    { name: 'Casey Turner', online: false },
+    { name: 'Jimmie  Wilson', online: false },
+    { name: 'Nathaniel Steele', online: false },
+    { name: 'Aubrey  Cole', online: false },
+    { name: 'Donnie  Summers', online: false },
+    { name: 'Kate  Myers', online: false },
+    { name: 'Priscilla Hawkins', online: false },
+    { name: 'Joe Barker', online: false },
+    { name: 'Lee Norman', online: false },
+    { name: 'Ebony Rice', online: false }
+  ];
+
+  //
+  // 'Forms' screen
+  //  
+  $scope.rememberMe = true;
+  $scope.email = 'me@example.com';
+  
+  $scope.login = function() {
+    alert('You submitted the login form');
+  };
+
+  // 
+  // 'Drag' screen
+  // 
+  $scope.notices = [];
+  
+  for (var j = 0; j < $scope.chatUsers.length; j++) {
+    var user = $scope.chatUsers[j];
+    $scope.notices.push({icon: 'envelope', message: 'Notice ' + j });
+  }
+
+  $scope.deleteNotice = function(notice) {
+    var index = $scope.notices.indexOf(notice);
+    if (index > -1) {
+      $scope.notices.splice(index, 1);
+    }
+  };
+});
